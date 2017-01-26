3639306e5fa43c28a9f158260ddd158c6245aa07
theodore.moke <theodore.moke@gmail.com>
2017-01-26 17:46:43 -0500

    Began nav and initial formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
 });
+
+gulp.task('useref', function() {
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('watch', ['sass'], function() {
   gulp.watch('src/scss/**/*.scss', ['sass']);
