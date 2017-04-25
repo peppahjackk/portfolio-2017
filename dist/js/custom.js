3dffd2883e046b105278243cd036aaa6f7258de2
Ted Moke <theodore.moke@gmail.com>
2017-04-25 11:37:47 -0400

    Removed hash add to location because NaN was being applied due to pixel offset

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -72,11 +72,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top - 64
-      }, 600, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash - 64;
-      });
+      }, 600);
     } // End if
   });
 });
