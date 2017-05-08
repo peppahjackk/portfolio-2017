b614cff18e1cdd454bd1819b231eceb3028dfdda
Ted Moke <theodore.moke@gmail.com>
2017-05-08 13:35:31 -0400

    Formats code

--- dist/php/process.php
+++ dist/php/process.php
@@ -20,63 +20,61 @@ if (!$result->success) {
 	$errorMSG .= "Please complete reCAPTCHA ";
 } 
 
-	//Name Field
-	if (empty($_POST["name"])) {
-		$errorMSG .= "Name is required ";
-	} else {
-		$name = $_POST["name"];
-	}
+//Name Field
+if (empty($_POST["name"])) {
+	$errorMSG .= "Name is required ";
+} else {
+	$name = $_POST["name"];
+}
 
-	//Email Field
-	if (empty($_POST["email"])) {
-		$errorMSG .= "Email is required ";
-	} else {
-		$email = $_POST["email"];
-	}
+//Email Field
+if (empty($_POST["email"])) {
+	$errorMSG .= "Email is required ";
+} else {
+	$email = $_POST["email"];
+}
 
-	//Inquiry Field
-	if (empty($_POST["inquiry"])) {
-		$errorMSG .= "Inquiry is required ";
-	} else {
-		$inquiry = $_POST["inquiry"];
-	}
+//Inquiry Field
+if (empty($_POST["inquiry"])) {
+	$errorMSG .= "Inquiry is required ";
+} else {
+	$inquiry = $_POST["inquiry"];
+}
 
-	//Phone, not required
-	$phone = $_POST["phone"];
+//Phone, not required
+$phone = $_POST["phone"];
 
 
-	$EmailTo = "theodore.moke@gmail.com";
-	$Subject = "Contact Form Inquiry";
-	//Your domain to avoid the host blocking what it thinks is spam
-	$EmailFrom = "web-support@tedmoke.com";
+$EmailTo = "theodore.moke@gmail.com";
+$Subject = "Contact Form Inquiry";
+//Your domain to avoid the host blocking what it thinks is spam
+$EmailFrom = "web-support@tedmoke.com";
 
-	// Creates template for email body
-	$Body = "";
-	$Body .= "Name: ";
-	$Body .= $name;
-	$Body .= "\n";
+// Creates template for email body
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
 
-	$Body .= "Email: ";
-	$Body .= $email;
-	$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
 
-	/*$headers = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-Transfer-Encoding: 8bit' . "\r\n";
-	$headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n"; */
+/*$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-Transfer-Encoding: 8bit' . "\r\n";
+$headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n"; */
 
-	$Body .= "Phone: ";
-	$Body .= $phone;
-	$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
 
-	$Body .= "Inquiry: ";
-	$Body .= $inquiry;
-	$Body .= "\n";
+$Body .= "Inquiry: ";
+$Body .= $inquiry;
+$Body .= "\n";
 
 $Headers = 'From: '.$EmailFrom."\r\n".
-     
-   'Reply-To: '.$email."\r\n" .
-     
-   'X-Mailer: PHP/' . phpversion();
+'Reply-To: '.$email."\r\n" .
+'X-Mailer: PHP/' . phpversion();
 
 // Sends Email
 if ($errorMSG == "") {$success = mail($EmailTo, $Subject, $Body, $Headers);}
